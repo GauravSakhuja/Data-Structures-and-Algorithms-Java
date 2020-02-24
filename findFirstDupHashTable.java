023851090820d1380790137077aaa04c125be6e2
GauravSakhuja <gaurav.sakhuja@gmail.com>
2020-02-24 00:53:24 +0000

    find first occurance of duplicate in array/ O(n) with Hashtable

---
+++ findFirstDupHashTable.java
@@ -0,0 +1,32 @@
+//find first occurance of any duplicate in array
+
+import java.util.*;
+
+class findFirstDupHashTable
+
+{
+  
+
+public static void main(String[] args)
+{
+  int[] arr= {2,5,5,2,9,6,5,4};
+  findFirstDupHashTable obj= new findFirstDupHashTable();
+  System.out.println(obj.findFirstDup(arr));
+}
+
+
+
+int findFirstDup(int[] arr)
+{
+  Hashtable<Integer,Integer> h = new Hashtable <Integer,Integer>();
+  for (int i=0; i<arr.length-1;i++)
+  {
+    if(!h.containsKey(arr[i]))
+      h.put(arr[i],0) ;
+    else 
+    return (int)arr[i];
+  }
+  return 10000;
+}//O(n)
+
+}
